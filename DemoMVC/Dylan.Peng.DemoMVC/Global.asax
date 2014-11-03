3a1b2d894626015a8f31fe7e0678f6a5493e4397
peng_dylan <peng_dylan@sina.com>
2014-11-03 22:31:36 +0800

    update

---
+++ DemoMVC/Dylan.Peng.DemoMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dylan.Peng.DemoMVC.MvcApplication" Language="C#" %>
---
+++ DemoMVC/Dylan.Peng.DemoMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dylan.Peng.DemoMVC.Startup))]
+namespace Dylan.Peng.DemoMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
